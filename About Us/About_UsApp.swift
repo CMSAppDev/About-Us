2f6da90a1fc21da79eeeb2a9192ec6a36590d23f
Daniel Poe <thed4nm4n@gmail.com>
2022-03-25 10:14:46 -0400

    Initial Commit

---
+++ About Us/About_UsApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_UsApp.swift
+//  About Us
+//
+//  Created by Daniel Jaye Poe (Student) on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct About_UsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
