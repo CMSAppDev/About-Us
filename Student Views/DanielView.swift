8603a85d26011e0b0974f4e80b188ef210ff209f
Noah McCampbell <noahmccamp@aol.com>
2022-03-29 11:50:27 -0400

    Daniel Test Conflict

--- Student Views/DanielView.swift
+++ Student Views/DanielView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct DanielView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("NO Daniel")
+        Text("no")
     }
 }
 
