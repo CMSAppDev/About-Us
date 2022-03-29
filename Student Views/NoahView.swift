96ded6b257c7807e1c92a6810279ad0787a96cf4
Noah McCampbell <noahmccamp@aol.com>
2022-03-29 11:47:00 -0400

    Initial NoahView Content Created.

--- Student Views/NoahView.swift
+++ Student Views/NoahView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct NoahView: View {
+    @State var onInit = false
+    @State var opacity = 0.0
+    init(){
+        
+        
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            
+            Spacer()
+            Text("Noah M").padding()
+            Text("Im a programmer and game developer, I also like making blender animations. I love learning new things and exploring new opportunities.").padding()
+            Text("This is in the Noah View").padding()
+            Spacer()
+            Button("Big Secret"){
+                withAnimation(.linear(duration: 100.0)) {
+                    opacity += 100.0
+                }
+            }.padding()
+            Image("debian")
+                .opacity(opacity)
+            Text("Im sorry... Debian is just better.")
+                .opacity(opacity)
+            Spacer()
+        }
     }
 }
 
