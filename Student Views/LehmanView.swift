9522aa9e947dfcd444478b0f82ee063e2d363055
Hans Lehman <hans.lehman@gmail.com>
2022-03-31 10:54:54 -0400

    Here is Lehman's View Controller with the additional assets to make it work.

--- Student Views/LehmanView.swift
+++ Student Views/LehmanView.swift
@@ -6,10 +6,37 @@
 //
 
 import SwiftUI
+import AVKit
+var name: Int = 1
+
+class PlayViewModel{
+    //private var name: Int = 1
+    private var audioPlayer: AVAudioPlayer!
+    func play(){
+        let sound = Bundle.main.path(forResource: String(name) , ofType: ".m4a")
+        self.audioPlayer = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        self.audioPlayer.play()
+    }
+}
 
 struct LehmanView: View {
+    
+    let vm = PlayViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+        Text("Hello, World!  This is Hans Lehman")
+        Text("Press on the image for some words of wisdom.")
+            Button
+            {
+                name = Int.random(in: 1...4)
+                self.vm.play()
+                
+             //Image("Lehman").resizable().scaledToFit() as! PrimitiveButtonStyleConfiguration
+            } label: {
+                Image("Lehman").resizable().scaledToFit()
+            }
+        //Image("Lehman").resizable().scaledToFit()
+        }
     }
 }
 
