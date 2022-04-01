171384c6965917273619001d468e609612f56151
ella <22trumaell@casdonline.org>
2022-04-01 10:57:07 -0400

    i finished my view

--- Student Views/EllaView.swift
+++ Student Views/EllaView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct EllaView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+         VStack{
+             Text("This is me")
+             Text("I look sooooo tired")
+            Image("Ella")
+                 .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+            
+            
+            
+        }
     }
 }
 
